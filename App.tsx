a61079eb5a7bee9bacc17ee0cbdcf83f1c3a0664
Weverton dos Reis <wevertondosreis@gmail.com>
2023-01-16 19:55:22 -0300

    Criando a SignUp

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import {  StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base'
+import { NativeBaseProvider, View } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
 
 import { THEME } from './src/theme';
 
 import { Loading } from '@components/Loading';
-import { Signin } from '@screens/Signin';
+import { SignIn } from '@screens/SignIn';
+import { SignUp } from '@screens/SignUp';
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
@@ -18,7 +20,7 @@ export default function App() {
         translucent
       
       />
-     {fontsLoaded ?  <Signin/> : <Loading/>}
+     {fontsLoaded ? <SignUp/> : <Loading/>}
      
     </NativeBaseProvider>
   );
