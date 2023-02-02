036d467468334fb3df4c9b78128f4bc01369d3cf
Weverton dos Reis <wevertondosreis@gmail.com>
2023-02-02 20:20:14 -0300

    Posicionando na vertical e horizontal

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,10 @@
-import { Center, Text } from "native-base";
+import { HomeHeader } from "@components/HomeHeader";
+import { VStack, Text } from "native-base";
 
-export function Home(){
-    return(
-        <Center flex={1}>
-            <Text>Home</Text>
-        </Center>
+export function Home() {
+    return (
+        <VStack flex={1}>
+            <HomeHeader />
+        </VStack>
     )
 }
