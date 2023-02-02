5a2fe7fcfe51896f791ccdf5686e96e14bff0dc5
Weverton dos Reis <wevertondosreis@gmail.com>
2023-02-02 18:57:03 -0300

    Tipando Rotas

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,9 +1,19 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-const { Navigator, Screen } = createNativeStackNavigator();
+import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 import { SignIn } from '@screens/SignIn';
 import { SignUp } from '@screens/SignUp';
+
+
+type AuthRoutes ={
+    signIn: undefined;
+    signUp: undefined;
+}
+
+export type AuthNavigatorRoutesProps = NativeStackNavigationProp<AuthRoutes>;
+
+
+const { Navigator, Screen } = createNativeStackNavigator<AuthRoutes>();
+
 
 export function AuthRoutes() {
     return (
