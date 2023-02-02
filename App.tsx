30988d20a1ae77403410075a18739a4cfb37d108
Weverton dos Reis <wevertondosreis@gmail.com>
2023-02-02 19:00:50 -0300

    Criando o contexto de navegação

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import { THEME } from './src/theme';
 import { Loading } from '@components/Loading';
 import { SignIn } from '@screens/SignIn';
 import { SignUp } from '@screens/SignUp';
+import { Routes } from '@routes/index';
 
 
 export default function App() {
@@ -20,7 +21,7 @@ export default function App() {
         translucent
       
       />
-     {fontsLoaded ? <SignUp/> : <Loading/>}
+     {fontsLoaded ? <Routes/> : <Loading/>}
      
     </NativeBaseProvider>
   );
