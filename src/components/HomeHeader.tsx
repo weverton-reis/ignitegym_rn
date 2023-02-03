f66190183bde8d80dc49755e28aa038f4877b108
Weverton dos Reis <wevertondosreis@gmail.com>
2023-02-03 17:32:10 -0300

    Finalização da interface

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,10 +1,20 @@
-import { Heading, HStack, Text, VStack } from 'native-base';
+import { TouchableOpacity } from 'react-native';
+import { Heading, HStack, Text, VStack, Icon } from 'native-base';
+import { MaterialIcons } from '@expo/vector-icons';
+
+import { UserPhoto } from './UserPhoto';
 
 export function HomeHeader() {
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
-      <VStack>
-      <Text color="gray.100" fontSize="md">
+      <UserPhoto
+        source={{ uri: 'https://github.com/rodrigorgtic.png' }}
+        size={16}
+        alt="Imagem do usuário"
+        mr={4}
+      />
+       <VStack flex={1}>
+        <Text color="gray.100" fontSize="md">
           Olá,
         </Text>
 
@@ -12,6 +22,15 @@ export function HomeHeader() {
           Rodrigo
         </Heading>
       </VStack>
+
+      <TouchableOpacity>
+        <Icon
+          as={MaterialIcons}
+          name="logout"
+          color="gray.200"
+          size={7}
+        />
+      </TouchableOpacity>
     </HStack>
   );
 }
