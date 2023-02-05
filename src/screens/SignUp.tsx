45bdba2a00474ccccfe04cf36710a1b3f62e3c1e
Weverton dos Reis <wevertondosreis@gmail.com>
2023-02-05 08:41:50 -0300

    Ajustando Heading

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -150,7 +150,7 @@ export function SignUp() {
                 <Button
                     title="Voltar para o login"
                     variant="outline"
-                    mt={24}
+                    mt={12}
                     onPress={handleGoBack}
 
                 />
